d42eb92e319152502cfe139b8ef131eb86347edd
yarwicsr <yarwicsr@miamioh.edu>
2022-05-03 13:51:24 -0400

    Upload New File

---
+++ Request.java
@@ -0,0 +1,16 @@
+public class Request {
+	public int floor;
+	public boolean isPriority;
+
+	public Request(int floor, boolean isPriority) {
+		// TODO Auto-generated constructor stub
+		this.floor = floor;
+		this.isPriority = isPriority;
+	}
+
+	public Request() {
+		this.floor = 1;
+		this.isPriority = false;
+		// TODO Auto-generated constructor stub
+	}
+}
