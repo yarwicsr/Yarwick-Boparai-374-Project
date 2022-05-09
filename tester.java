080bf6e8cb7b059f5686372871c39649eeb9202e
yarwicsr <yarwicsr@miamioh.edu>
2022-05-08 21:26:38 -0400

    Update tester.java

--- tester.java
+++ tester.java
@@ -4,17 +4,22 @@ import java.util.List;
 class Tester {
 	public static void main(String[] args) {
 		// elevator location
-		int location = 1;
-		// direction of elevator
-		// true = up
-		// false = down
+		int location = 10;
+		// direction of elevator true = up false = down
 		boolean direction = true;
-		List<Request> people = new ArrayList<>();
-		Request Bob = new Request(2, false);
-		people.add(Bob);
+		List<Request> requests = new ArrayList<>();
 
+    // continue to make and add requests in this format
+		Request Bob = new Request(2, false);
+		requests.add(Bob);
+		
 		Elevator elevator = new Elevator(location, direction);
-		int time = elevator.ElevatorTime(people, elevator);
-		System.out.println(time);
+		System.out.println("Elevator location: " + location + " | Elevator direction up/down (true/false): " + direction);
+
+		long start = System.currentTimeMillis();
+		elevator.ElevatorTime(requests, elevator);
+		long end = (System.currentTimeMillis() - start);
+		System.out.println("Completed in " + end + " ms");
+
 	}
 }
